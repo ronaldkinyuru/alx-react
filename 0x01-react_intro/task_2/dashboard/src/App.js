ae0c6279d8a9425cabb14a62c812711f5c7dc26a
ronaldkinyuru <ronaldkinyuru@gmail.com>
2024-09-13 12:42:42 +0300

    htmlFor="id"

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,30 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+import {getFullYear, getFooterCopy} from './utils';
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img src={logo} alt="logo"/>
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email"/>
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password"/>
+        <br />
+        <button>OK</button>
+
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)} </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
